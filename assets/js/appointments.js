dbb02e628999eadbd94c8c9c08fb0c70ec4f771f
tlatimore <tlatimore0551@yahoo.com>
2024-09-07 21:08:36 -0400

    updated appointments page

--- assets/js/appointments.js
+++ assets/js/appointments.js
@@ -1,33 +1,25 @@
 const doneButton = document.querySelector('#done-button');
 let box = document.querySelector('.box');
 let appointments = document.querySelector('.appointment-list');
-//let names = ['John Doe', 'Jane Doe', 'James Doe', 'Jenny Doe'];
 
 doneButton.addEventListener('click', () => {
-  alert('Done button clicked');
-
   //redirect to the landing page
     window.location.href = 'index.html';
 });
 
 //get the data from the local storage
-const getData = window.location.search;
-const urlParams = new URLSearchParams(getData);
+let storedData = JSON.parse(localStorage.getItem('appointments')) || [];
 
-let name = urlParams.get('name');
-let date = urlParams.get('date');
-let time = urlParams.get('time');
+console.log(storedData);
 
-console.log(name);
+for (let i = 0; i < storedData.length; i++) {
+  console.log(storedData[i]);
 
-
-
-for (let i = 0; i < apptInfo.length; i++) {
-    appointments.innerHTML += `<div class="box"> <h2>Appointment ${i+1} </h2> 
-    <p>Appointment date: </p>
-     <p>Appointment time: ${names[i]} </p>
-      <p>Appointment type: Consultation</p>
-       <p>Appointment status: Done</p>
+    appointments.innerHTML += `<div class="box"> 
+    <h2>Appointment ${i+1} </h2>
+    <p>Appointment name: ${storedData[i].name} </p> 
+    <p>Appointment date: ${storedData[i].date} </p>
+     <p>Appointment time: ${storedData[i].time} </p>
         </div>`;
 };
 
